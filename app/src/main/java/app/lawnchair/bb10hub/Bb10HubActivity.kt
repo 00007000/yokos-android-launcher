ba4d811c9f2a3ebe6d987eeafa1749b343f50fac
00007000 <yakuphanordu@gmail.com>
2026-06-09 12:57:56 +0000

    fix: Ultra-simple Activity without Compose

--- app/src/main/java/app/lawnchair/bb10hub/Bb10HubActivity.kt
+++ app/src/main/java/app/lawnchair/bb10hub/Bb10HubActivity.kt
@@ -1,30 +1,16 @@
 package app.lawnchair.bb10hub
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.sp
+import android.widget.TextView
 
-class Bb10HubActivity : ComponentActivity() {
+class Bb10HubActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color(0xFF1A1A1A)),
-                contentAlignment = Alignment.Center
-            ) {
-                Text("BB10 Hub", color = Color.White, fontSize = 24.sp)
-            }
-        }
+        val textView = TextView(this)
+        textView.text = "BB10 Hub"
+        textView.setTextColor(android.graphics.Color.WHITE)
+        textView.setBackgroundColor(android.graphics.Color.BLACK)
+        setContentView(textView)
     }
 }
